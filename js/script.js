b9382b4e54a392f1541b5bb3bc37cbc9426ada8a
olivierhsta <olivier@hassaoui.com>
2019-01-14 16:28:49 -0500

    Call to API and loading animation

--- js/script.js
+++ js/script.js
@@ -36,4 +36,34 @@ $(function(){
 
     $('#reponse-1').css('background-color', '#000');
 
+
 });
+
+function getQuote(){
+    $('#spinner').removeClass('d-none');
+    $('#btn-new-quote').addClass('not-active');
+
+    $.ajax( {
+      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+      success: function(data) {
+        var post = data.shift(); // The data is an array of posts. Grab the first one.
+        if (post.content.length > 60) {
+            getQuote();
+        } else {
+            $('#spinner').addClass('d-none');
+            $('#btn-new-quote').removeClass('not-active');
+
+            $('#quote-title').text(post.title);
+            $('#quote-content').html(post.content);
+
+            // If the Source is available, use it. Otherwise hide it.
+            if (typeof post.custom_meta !== 'undefined' && typeof post.custom_meta.Source !== 'undefined') {
+                $('#quote-source').html('Source:' + post.custom_meta.Source);
+            } else {
+                $('#quote-source').text('');
+            }
+        }
+      },
+      cache: false
+    });
+}
